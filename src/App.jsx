957afdba3a82dc5df449f9f858a7bba553ffab65
SajeedAninda <sajaninda@gmail.com>
2023-09-14 19:41:41 +0600

    Created Single Card Layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import AllCourses from './Components/AllCourses/AllCourses'
 function App() {
 
   return (
-    <>
+    <div>
+      <h1 className="text-[#1C1B1B] font-bold text-3xl my-6 text-center">Course Registration</h1>
       <AllCourses></AllCourses>
-    </>
+    </div>
   )
 }
 
--- src/Components/AllCourses/AllCourses.jsx
+++ src/Components/AllCourses/AllCourses.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
+import SingleCourse from "../SingleCourse/SingleCourse";
 
 export default function AllCourses() {
     let [AllCourses, setCourses] = useState([]);
     useEffect(() => {
         fetch("./CourseData.json")
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => setCourses(data));
     }, [])
 
     return (
         <div>
-
+            <SingleCourse></SingleCourse>
         </div>
     )
 }
